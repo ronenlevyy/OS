e2edf166d673538d03f81928a3ea4005266480df
ronenlevyy <ronen.levy@mail.huji.ac.il>
2025-04-11 17:09:13 +0300

    Add files via upload

--- OS_Ex1/Resources/plot_example.py
+++ OS_Ex1/Resources/plot_example.py
@@ -1,21 +1,24 @@
-# OS 24 EX1
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv(r"C:\path\to\output.csv")
+data = pd.read_csv(r"/cs/usr/ronenlevy133/Desktop/os_2025/ex1/OS_Ex1/Resources/resulted2.csv")
 data = data.to_numpy()
 
-l1_size = 0
-l2_size = 0
-l3_size = 0
+l1_size = 32*1024
+l2_size = 256*1024
+l3_size = 9*1024*1024
+
+pp = 2414534656
 
 plt.plot(data[:, 0], data[:, 1], label="Random access")
 plt.plot(data[:, 0], data[:, 2], label="Sequential access")
 plt.xscale('log')
 plt.yscale('log')
-plt.axvline(x=l1_size, label="L1 (?? ?iB)", c='r')
-plt.axvline(x=l2_size, label="L2 (?? ?iB)", c='g')
-plt.axvline(x=l3_size, label="L3 (?? ?iB)", c='brown')
+plt.axvline(x=l1_size, label="L1 (32 KiB)", c='r')
+plt.axvline(x=l2_size, label="L2 (256 KiB)", c='g')
+plt.axvline(x=l3_size, label="L3 (9 MiB)", c='brown')
+plt.axvline(x=pp, label="pp", c='blue')
 plt.legend()
 plt.title("Latency as a function of array size")
 plt.ylabel("Latency (ns log scale)")
